abb8a2df1cc0a58d39d2ced91d268020c1b38bad
soleyman <shahirsoleyman@yahoo.com>
2021-11-25 14:07:05 +0000

    Created react app and updated text

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         <code>Our App is running on AWS Elastic Beanstalk</code> 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React Rocks
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
